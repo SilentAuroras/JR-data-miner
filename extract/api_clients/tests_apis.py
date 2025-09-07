b5093b6ae5d4296a46ca86dd162c451db6f0c979
SilentAuroras <194649339+SilentAuroras@users.noreply.github.com>
2025-09-06 21:10:40 -0600

    Tests add asserts and quake client

--- extract/api_clients/tests_apis.py
+++ extract/api_clients/tests_apis.py
@@ -1,9 +1,22 @@
-import weather_client
+# General Imports
 import pandas as pd
+import os
+
+# Import API clients
+import weather_client
+import earthquake_client
 
 # Weather Client Tests - Tokyo Station and Kyoto
 coordinates = [(35.675163966, 139.766830266), (34.98561, 135.758915)]
-created_uuid = weather_client.get_weather_forcast(coordinates)
-filename = f"weather_{created_uuid}.parquet"
-df = pd.read_parquet(filename)
+weather_uuid = weather_client.get_weather_forcast(coordinates)
+weather_filename = f"weather_{weather_uuid}.parquet"
+assert os.path.exists(weather_filename), f"{weather_filename} not created"
+df = pd.read_parquet(weather_filename)
+print(df)
+
+# Earthquake Client Tests
+quake_uuid = earthquake_client.get_earthquake_events()
+quake_filename = f"earthquake_{quake_uuid}.parquet"
+assert os.path.exists(quake_filename), f"{quake_filename} not created"
+df = pd.read_parquet(quake_filename)
 print(df)
